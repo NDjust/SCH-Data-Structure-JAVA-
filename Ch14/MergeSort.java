5954040a07a02a6e419d7141c511c76a5a821a02
Nathan <skeks463@gmail.com>
2019-12-07 22:05:33 +0900

    Add Ch14 Sorting Algorithms

--- Ch14/MergeSort.java
+++ Ch14/MergeSort.java
@@ -1,5 +1,71 @@
 package Ch14;
 
 public class MergeSort {
+    public static void main(String args[]) {
+        int a[] = {69, 10, 30, 2, 16, 8, 31, 22};
+        int size = a.length;
 
+        MergeSorting S = new MergeSorting();
+        System.out.printf("\n정렬할 원소 : ");
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf(" %d", a[i]);
+        }
+
+        System.out.println();
+        S.Sort(a, 0, size - 1);
+    }
+}
+
+class MergeSorting {
+
+    private int sorted[] = new int[30];
+
+    public void merge(int a[], int m, int middle, int n) {
+        int i, j, k, t;
+        int size = a.length;
+
+        i = m;
+        j = middle + 1;
+        k = m;
+
+        while (middle >= i && j <= n) {
+            if (a[i] <= a[j]) {
+                sorted[k] = a[i++];
+            } else {
+                sorted[k] = a[j++];
+            }
+            k++;
+        }
+
+        if (i > middle) {
+            for (t = j; t <= n; t++, k++) {
+                sorted[k] = a[t];
+            }
+        } else {
+            for (t = i; t <= middle; t++, k++) {
+                sorted[k] = a[t];
+            }
+        }
+
+        for (t = m; t <= n; t++) {
+            a[t] = sorted[t];
+        }
+        System.out.printf("\n 병합 정렬 >> ");
+
+        for (t = 0; t < size; t++) {
+            System.out.printf("%3d ", a[t]);
+        }
+    }
+
+    public void Sort (int a[], int m, int n){
+        int middle;
+        if (m < n) {
+            middle = (m + n) / 2;
+            Sort(a, m, middle);
+            Sort(a, middle + 1, n);
+            merge(a, m, middle, n);
+        }
+
+    }
 }
---
+++ Ch14/TreeS.java
@@ -0,0 +1,72 @@
+package Ch14;
+
+public class TreeS {
+    public static void main(String[] args) {
+        int[] arr = { 50, 30, 70, 15, 7, 62, 22, 35, 87, 31 };
+        TreeSort TS = new TreeSort(arr[0]);
+
+        System.out.printf("정렬 전 : ");
+        printArray(arr);
+        TS.Sorting(arr);
+        System.out.printf("정렬 후 : ");
+        printArray(arr);
+
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node (int value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
+class TreeSort {
+    Node node;
+
+    TreeSort(int value) {
+        node = new Node(value);
+    }
+    int count = 0;
+
+    public void Sorting(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            insertBST(node, arr[i]);
+            inOrder(node, arr);
+        }
+    }
+
+    public Node insertBST(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = insertBST(node.left, value);
+        } else if (value > node.value) {
+            node.right = insertBST(node.right, value);
+        }
+
+        return node;
+    }
+
+    public void inOrder(Node node, int arr[]) {
+        if (node != null) {
+            inOrder(node.left, arr);
+            arr[count++] = node.value;
+            inOrder(node.right, arr);
+        }
+    }
+
+}
